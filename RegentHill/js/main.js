ce2b6801626e65f0825f1dd1996f1fbb0d1c1782
telabrams <telabrams@gmail.com>
2016-10-21 14:35:21 +0300

    Add files via upload

---
+++ RegentHill/js/main.js
@@ -0,0 +1,107 @@
+var officeData = { 'panorama': [
+{ 
+
+//1st panorama
+    'mapFile': 'textures/3d_Model.jpg',
+    'navMarkers': [
+      { 'target': 1,
+        'x': 497.0484355184786,
+        'y': -51.396268393623394,
+        'z': -17.357313842974687
+      },
+    ],
+    'markers': [
+      {'person': 'it',
+        'x': 455.0522836660948,
+        'y': -58.33536854966658,
+        'z': 198.80745435331792 
+      },
+      { 'person': 'bd',
+        'x': 404.3501347961816,
+        'y': -62.66661678215218,
+        'z': -287.3566836379287 
+      }
+    ]
+    
+  }, //end 1st panorama
+  { 
+
+//1st panorama
+    'mapFile': 'textures/mg_3010_3053.jpg',
+    'navMarkers': [
+      { 'target': 0,
+        'x': -496.80219781041825,
+        'y': 16.577589194263247,
+        'z': 53.969989690977606
+      },
+      { 'target': 2,
+        'x': 371.50393644709794 ,
+        'y': -9.59872119984476,
+        'z': 334.5036468495347
+      },
+    ],
+    'markers': [
+      {'person': 'darkroom',
+        'x': -216.82921204357737,
+        'y': 0.8726641829491307,
+        'z': -450.5378244518269 
+      },
+      { 'person': 'exit',
+        'x': 497.14559206230575,
+        'y': -23.55322535482139,
+        'z': -47.86967587528129
+      }
+    ]
+    
+  }, //end 1st panorama
+  {
+//2nd panorama
+    'mapFile': 'textures/4seasonsfinal3.jpg',
+    'navMarkers': [
+      { 'target': 0,
+        'x': 350.55335882972173,
+        'y': -65.065896266279868,
+        'z': 300.22722517927485
+      }
+    ],
+    'markers': [
+      { 'person': 'DaVinci',
+        'x': 186.35642970111687,
+        'y': 19.193904543760027,
+        'z': -463.57618051127014
+      }
+    ]
+  } //end 2nd panorama
+  
+  ],
+
+//end panorama
+
+  'people':{
+    'it': {
+      'name': 'The House of IT Department',
+      'role': '<iframe width="560" height="315" src="https://www.youtube.com/embed/JRdRF8fnKeM" frameborder="0" allowfullscreen></iframe>',
+      'image': 'img/no_image.jpg'
+    },
+    'bd': {
+      'name': 'School of Business and Management',
+      'role': 'Director - Julia Doe',
+      'image': 'img/julia.jpg'
+    },
+    'darkroom': {
+      'name': 'Public Library',
+      'role': 'This is our proud public library with 2.500.000 books.',
+      'image': 'img/no_image.jpg'
+    },
+    'exit': {
+      'name': 'Living Plant',
+      'role': 'Every student needs fresh air. Our living plants will produce it.',
+      'image': 'img/no_image.jpg'
+    },
+    'DaVinci': {
+      'name': 'Super VIP Jacuzee.',
+      'role': 'Repaired by Yanto.',
+      'image': 'img/yanto.jpg'
+    }
+  } 
+};
