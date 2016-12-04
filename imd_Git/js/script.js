ee48b2288c0f9d7e2fe3087c1c22b1aac2de06ec
telabrams <telabrams@gmail.com>
2016-12-04 14:13:27 +0200

    Add files via upload

---
+++ imd_Git/js/script.js
@@ -0,0 +1,19 @@
+
+jQuery(document).ready(function(){
+
+    // languages toggle
+
+    jQuery('.languages li:first-child').on('click', function(){
+        jQuery('.languages li').toggleClass('open_lang');
+
+        return false;
+    });
+
+    // search toggle
+
+    jQuery('.search i').on('click', function(){
+        jQuery(this).prev().fadeToggle(100);
+    });
+
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzY3JpcHQuanMiXSwic291cmNlc0NvbnRlbnQiOlsiXHJcbmpRdWVyeShkb2N1bWVudCkucmVhZHkoZnVuY3Rpb24oKXtcclxuXHJcbiAgICAvLyBsYW5ndWFnZXMgdG9nZ2xlXHJcblxyXG4gICAgalF1ZXJ5KCcubGFuZ3VhZ2VzIGxpOmZpcnN0LWNoaWxkJykub24oJ2NsaWNrJywgZnVuY3Rpb24oKXtcclxuICAgICAgICBqUXVlcnkoJy5sYW5ndWFnZXMgbGknKS50b2dnbGVDbGFzcygnb3Blbl9sYW5nJyk7XHJcblxyXG4gICAgICAgIHJldHVybiBmYWxzZTtcclxuICAgIH0pO1xyXG5cclxuICAgIC8vIHNlYXJjaCB0b2dnbGVcclxuXHJcbiAgICBqUXVlcnkoJy5zZWFyY2ggaScpLm9uKCdjbGljaycsIGZ1bmN0aW9uKCl7XHJcbiAgICAgICAgalF1ZXJ5KHRoaXMpLnByZXYoKS5mYWRlVG9nZ2xlKDEwMCk7XHJcbiAgICB9KTtcclxuXHJcbn0pOyJdLCJmaWxlIjoic2NyaXB0LmpzIn0=
